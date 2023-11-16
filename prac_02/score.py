2342610e5bb0549ef5b5f328bf763002855c7052
jkteo <junkai.teo@my.jcu.edu.au>
2023-11-16 14:45:39 +0800

    Refactor broken score to use functions

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,9 +1,22 @@
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score > 50:
-    print("Pass")
-else:
-    print("Bad")
+import random
+
+
+def main():
+    score = float(input("Enter score: "))
+    print(score_result(score))
+    print(score_result(random.randint(0, 100)))
+
+
+def score_result(score):
+    if score < 0 or score > 100:
+        result = "Invalid score"
+    elif score >= 90:
+        result = "Excellent"
+    elif score > 50:
+        result = "Pass"
+    else:
+        result = "Bad"
+    return result
+
+
+main()
