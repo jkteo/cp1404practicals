4774a1a6c2bcae4707e2c028a9c926f737eb1267
jkteo <junkai.teo@my.jcu.edu.au>
2023-11-27 17:39:32 +0800

    Add Prac 04 files

---
+++ prac_04/list_exercises.py
@@ -0,0 +1,19 @@
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+numbers = []
+for i in range(5):
+    number = int(input("Enter numbers: "))
+    numbers.append(number)
+print(f"The first number is {numbers[0]}")
+print(f"The last number is {numbers[-1]}")
+print(f"The smallest number is {min(numbers)}")
+print(f"The largest number is {max(numbers)}")
+print(f" The first number is {sum(numbers) / len(numbers)}")
+
+username = input("Enter username: ")
+if username in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
+
--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -7,23 +7,38 @@ FILENAME = "subject_data.txt"
 
 
 def main():
-    data = get_data()
-    print(data)
+    input_file = get_data()
+    data = print_data(input_file)
+    print_details(data)
 
 
 def get_data():
+    input_file = open(FILENAME, "r")
+    data = input_file.readlines()
+    input_file.close()
+    return data
+
+
+def print_data(input_file):
     """Read data from file formatted like: subject,lecturer,number of students."""
-    input_file = open(FILENAME)
+    part = []
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
+        # print(line)  # See what a line looks like
+        # print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
+        # print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
-    input_file.close()
+        # print(parts)  # See if that worked
+        # print("----------")
+        part.append(parts)
+    return part
+
+
+def print_details(parts):
+    """Read data from parts formatted: CP1401 is taught by Ada Lovelace and has 192 students"""
+    for line in parts:
+        print(f"{line[0]} is taught by {line[1]} and has {line[2]} students")
 
 
 main()
