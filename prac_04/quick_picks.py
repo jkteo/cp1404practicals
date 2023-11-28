46d45d396bebf334aedcff33458081ad8c5de84d
jkteo <junkai.teo@my.jcu.edu.au>
2023-11-28 11:27:03 +0800

    Add Prac 04 file

---
+++ prac_04/quick_picks.py
@@ -0,0 +1,29 @@
+import random
+
+LENGTH = 6
+MIN_NUMBER = 1
+MAX_NUMBER = 45
+
+
+def main():
+    lines = int(input("How many quick picks? "))
+    while lines < 0:
+        print("Invalid amount, try again")
+        lines = int(input("How many quick picks? "))
+
+    for i in range(lines):
+        data = []
+        generate_random(data)
+        print(" ".join(f"{number:2}" for number in data))
+
+
+def generate_random(data):
+    for i in range(LENGTH):
+        random_no = random.randint(MIN_NUMBER, MAX_NUMBER)
+        while random_no in data:
+            random_no = random.randint(MIN_NUMBER, MAX_NUMBER)
+        data.append(random_no)
+    return data.sort()
+
+
+main()
